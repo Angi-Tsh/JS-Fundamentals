0d1f3782db38cd1eb5ebaa11c2140318b2a9b10c
Angello <kktshoga@gmail.com>
2025-05-25 16:13:25 +0900

    Add 6-multi_languages_loop.js solution with loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,9 +3,16 @@ const messages = [
   "Python is cool",
   "JavaScript is amazing"
 ];
+
 let outputString = "";
 let i = 0;
+
 while (i < messages.length) {
-     outputString += messages[i]; outputString += '\n';i++;}
-     
+  outputString += messages[i];
+  outputString += '\n';
+  i++;
+}
+
+outputString = outputString.slice(0, -1); // This line is crucial for the length
+
 console.log(outputString);
