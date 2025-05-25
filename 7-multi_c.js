4f4f8aadd1d24ee1e2cc350d1437565344edbce5
Angello <kktshoga@gmail.com>
2025-05-25 16:21:03 +0900

    feat: Add 7-multi_c.js solution for printing 'C is fun' x times

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+const numOccurrencesArg = process.argv[2];
+const x = parseInt(numOccurrencesArg);
+if (isNaN(x)) {
+  console.log("Missing number of occurrences"); // First allowed console.log call
+} 
+else { let outputString = "";
+  let count = 0;
+  while (count < x) {
+    outputString += "C is fun";
+    outputString += '\n'; count++;
+  } 
+  console.log(outputString);
+}
