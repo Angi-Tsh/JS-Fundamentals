b6a975de527a0354d7a4a9334193b6078d6aa1bc
Angello <kktshoga@gmail.com>
2025-05-25 16:30:17 +0900

    Added

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,19 @@
 const numOccurrencesArg = process.argv[2];
 const x = parseInt(numOccurrencesArg);
+
 if (isNaN(x)) {
-  console.log("Missing number of occurrences"); // First allowed console.log call
-} 
-else { let outputString = "";
+  console.log("Missing number of occurrences");
+} else {
+  let outputString = "";
   let count = 0;
+
   while (count < x) {
     outputString += "C is fun";
-    outputString += '\n'; count++;
-  } outputString = outputString.slice(0, -1);
-  console.log(outputString);
+    outputString += '\n';
+    count++;
+  }
+
+  outputString = outputString.slice(0, -1);
+
+  outputString && console.log(outputString);
 }
