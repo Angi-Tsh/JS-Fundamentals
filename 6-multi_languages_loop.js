9e07523e19242c5a13f87fee0775c9dc84ca6a98
Angello <kktshoga@gmail.com>
2025-05-25 16:09:34 +0900

    feat: Add 6-multi_languages_loop.js solution with explicit loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+let outputString = "";
+let i = 0;
+while (i < messages.length) {
+     outputString += messages[i]; outputString += '\n';i++;}
+     
+console.log(outputString);
